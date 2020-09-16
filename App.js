401ebd923b9399111e2ac516195868af3679a66b
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-16 13:48:51 +0700

    Basics of React Native

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,Image,Alert, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
+  const [colorChange,setColorChange] = useState(false)
+  const handleClick =()=>{
+    setColorChange(!colorChange)
+    // Alert.alert('my Title', 'My message',[
+    //   {text: 'yes', onPress: ()=>console.log('yes') },
+    //   {text: 'no'}
+    // ])
+    Alert.prompt('myTitle', 'my message', text =>console.log(text))
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello, world!</Text>
+      <Image 
+        source={{
+          uri: 'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
+          height:200,
+          width:200,
+        }}
+      />
+      <Button title='click me' color={colorChange ? 'orange': 'blue'} onPress={()=> handleClick()}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -19,3 +36,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// padding: Platform.os === 'andriod' ? statusBar.currentHeight :0
