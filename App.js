04121bfc5a7b7da6b76a88f4ceada693ecd9c9c4
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2020-09-16 14:44:05 +0700

    Flex basics

--- App.js
+++ App.js
@@ -1,31 +1,64 @@
 
 import React from 'react';
 import { SafeAreaView, StyleSheet,View} from 'react-native';
-import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
 export default function App() {
 
-  console.log(useDeviceOrientation())
-  const {landscape} =useDeviceOrientation();
+
   return (
-    <SafeAreaView style={styles.container}>
+   
       <View
         style={{
-          backgroundColor: 'blue',
-          height: landscape ? '100%' : '70%', 
-          width: '100%'
+          backgroundColor: 'white',
+          flex:1,
+          flexDirection:'row',
+          justifyContent:'center',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          alignContent: 'center'
         }}
-      ></View>
-    </SafeAreaView>
+      >
+        <View 
+          style={{
+            backgroundColor: 'orange',
+            height:100,
+            width: 100,
+          
+          }}
+        />
+        <View 
+          style={{
+            backgroundColor: 'blue',
+            height:100,
+            width: 100
+          }}
+        />
+        <View 
+          style={{
+            backgroundColor: 'red',
+            height:100,
+            width: 100
+          }}
+        />
+        <View 
+          style={{
+            backgroundColor: 'gray',
+            height:100,
+            width: 100
+          }}
+        />
+        <View 
+          style={{
+            backgroundColor: 'black',
+            height:100,
+            width: 100
+          }}
+        />
+      </View>
+     
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+
 
 // padding: Platform.os === 'andriod' ? statusBar.currentHeight :0
